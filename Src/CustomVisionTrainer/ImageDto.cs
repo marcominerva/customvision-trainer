529ff15378e8cee82e4113aa5c89f8433186cf9e
Andrea Tosato <andrea.tosato@hotmail.it>
2019-01-10 07:00:59 +0100

    Batch upload

---
+++ Src/CustomVisionTrainer/ImageDto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Cognitive.CustomVision.Training.Models;
+
+namespace CustomVisionTrainer
+{
+    public class ImageDto
+    {
+        public string FullName { get; set; }
+        public string FileName { get; set; }
+        public byte[] Content { get; set; }
+        public Tag Tag { get; set; }
+    }
+}
